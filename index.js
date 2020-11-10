316a215e34809012389d8e3f5910d076f9d255c1
fayez bdair <f.bdair@unitone.ps>
2020-11-10 17:22:23 +0200

    first commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = 3000
 
 // Tell express to use body-parser's JSON parsing
 app.use(bodyParser.json())
-app.post("/hook", (req, res) => {
+app.post("/hook/instagram", (req, res) => {
   console.log(req.body)
   res.status(200).end();
 });
