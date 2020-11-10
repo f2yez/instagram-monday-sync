1d44d6786b46d42a0c6664ed5252b0dbe56a7313
fayez bdair <f.bdair@unitone.ps>
2020-11-10 17:20:16 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Require express and body-parser
+const express = require("express")
+const bodyParser = require("body-parser")
+
+// Initialize express and define a port
+const app = express()
+const PORT = 3000
+
+// Tell express to use body-parser's JSON parsing
+app.use(bodyParser.json())
+app.post("/hook", (req, res) => {
+  console.log(req.body)
+  res.status(200).end();
+});
+
+// Start express on the defined port
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
