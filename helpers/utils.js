860a88a0bc78455a2e3c4f3edd8bf292d32103f5
fayez bdair <f.bdair@unitone.ps>
2020-11-10 18:49:35 +0200

    fayez [ADD] return string with get method

---
+++ helpers/utils.js
@@ -0,0 +1,13 @@
+// someone comments on one of your photos or mentions you in a comment on Instagram
+function generateNewCommentMsg(text) {
+    return "Someone comments on one of your photos";
+}
+
+function generateNewMentionsMsg() {
+    return "Someone mentions you in a comment";
+}
+
+module.exports = {
+    generateNewCommentMsg,
+    generateNewMentionsMsg
+}
--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 // Require express and body-parser
 const express = require("express")
 const bodyParser = require("body-parser")
-
+const { generateNewCommentMsg } = require('./helpers/utils');
 // Initialize express and define a port
 const app = express()
 const PORT = process.env.PORT || 3000
 
+// Register Body parser to my app to i can parseing request and response
 app.use(bodyParser.json())
-app.get("/hook/instagram", (req, res) => {
-  let VERIFY_TOKEN = "@1234@56789@@@";
 
+// Add Get http method for webhook callback to verify my callback url
+app.get("/hook/instagram", (req, res) => {
+  // My Verification token same into the 
+  let VERIFY_TOKEN = "@1234@56789@@@";
+  // Facebook callback request params
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
+
   if (mode && token) {
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
       console.log('WEBHOOK_VERIFIED');
@@ -24,7 +29,14 @@ app.get("/hook/instagram", (req, res) => {
 });
 
 app.post("/hook/instagram", (req, res) => {
-  console.log(req.body)
+  const { field, value } = req.body;
+  let slackMessage = '';
+  if (field == 'comments') {
+    slackMessage = generateNewCommentMsg(value.text);
+  } else if (field == 'mentions') {
+
+  }
+  console.log(slackMessage);
   res.status(200).end();
 });
 
