397c41a3669c1e86f4222bf6be7dc6db6fb40a7a
fayez bdair <f.bdair@unitone.ps>
2020-12-09 10:49:38 +0200

    fayez [ADD] add wix sync

--- controllers/hookController.js
+++ controllers/hookController.js
@@ -1,7 +1,14 @@
 const _ = require('lodash');
 
-async function receiver (req, res) {
+async function getReceiver (req, res) {
     console.log('receiverreceiverreceiver');
+    res.send('Hello Webhook');
+    res.end();
+}
+
+
+async function receiver (req, res) {
+    console.log('req.body', req.body);
     res.send('Hello Webhook');
     res.end();
 }
--- routes/hook/index.js
+++ routes/hook/index.js
@@ -2,6 +2,7 @@ var hookController = require('../../controllers/hookController.js');
 var express = require('express');
 var router = express.Router();
 
-router.get('/receiver', hookController.receiver);
+router.get('/receiver', hookController.getReceiver);
+router.post('/receiver', hookController.receiver);
 
 module.exports = router;
