b3ec97e35be3dbe0e25689a2df0ab3fb4bef5eb2
fayez bdair <f.bdair@unitone.ps>
2020-12-09 15:32:50 +0200

    fayez [FIX] fix reciever controller

--- controllers/hookController.js
+++ controllers/hookController.js
@@ -9,22 +9,30 @@ async function getReceiver (req, res) {
 
 
 async function getItem(itemId) {
-    await monday.api(`query { items (ids: [${itemId}]) {
-        id, name,
-        column_values {
-            id
-            title
-            value
-          }
-    }}`).then(res => {
-        console.log(res.data.items[0].column_values);
-    });
+    let item = null;
+    try {
+        const items = await monday.api(`query { items (ids: [${itemId}]) {
+            id, name,
+            column_values {
+                id
+                title
+                value
+              }
+        }}`);
+        if  (items && items.data && res.data.items.length > 0) {
+            item = res.data.items[0]; // column_values structure is [{id, title, value}]
+        }
+    } catch (error) {
+        console.log('Error while get element', error.message);
+    }
+    return item;
 }
 
 
 async function receiver (req, res) {
     const { action } = req.params;
     const { payload: { inputFields: { itemId } } }  = req.body;
+    console.log('Payload =>', req.body.payload);
     console.log('ItemId =>', itemId);
     console.log('Action =>', action);
     const item = await getItem(itemId);
