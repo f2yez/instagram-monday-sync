5db702be3fd344aec4d6a1597a018d3cb4af6546
fayez bdair <f.bdair@unitone.ps>
2020-12-15 12:39:47 +0200

    fayez [ADD] add adapter

--- routes/adaptor/schemas/index.js
+++ routes/adaptor/schemas/index.js
@@ -1,5 +1,5 @@
 
-var schemasController = require('../../controllers/schemasController');
+var schemasController = require('../../../controllers/schemasController');
 var express = require('express');
 var router = express.Router();
 
